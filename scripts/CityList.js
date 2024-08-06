0fc58bf85b22d4195e657e3d27afd65822d68eec
Daniel Henderson <djhindu17@gmail.com>
2024-08-06 12:06:07 -0500

    added for..of explanation of cityList

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,29 +1,44 @@
-import { getWalkers, getCities } from "./database.js"
+import { getWalkers, getCities } from "./database.js";
 
-const walkers = getWalkers()
-const cities = getCities()
+const walkers = getWalkers();
+const cities = getCities();
 
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const cityTarget = clickEvent.target
-        if (cityTarget.dataset.type === "city") {
-            const cityId = parseInt(cityTarget.dataset.cityid)
-            const walkersInCity = walkers.filter(walker => walker.cityId === cityId)
-            const walkerNames = walkersInCity.map(walker => walker.name).join(", ")
-            window.alert(`${walkerNames} is servicing this city.`)
-        }
-    }
-)
+document.addEventListener("click", (clickEvent) => {
+  const cityTarget = clickEvent.target;
+  if (cityTarget.dataset.type === "city") {
+    const cityId = parseInt(cityTarget.dataset.cityid);
+    const walkersInCity = walkers.filter((walker) => walker.cityId === cityId);
+    const walkerNames = walkersInCity.map((walker) => walker.name).join(", ");
+    window.alert(`${walkerNames} is servicing this city.`);
+  }
+});
+//     const cityTarget = clickEvent.target;
+//     if (cityTarget.dataset.type === "city") {
+//     const cityId = parseInt(cityTarget.dataset.cityid);
+//     const walkersInCity = [];
+//     const walkerNames = [];
+
+//     for (const walker of walkers) {
+//     if (walker.cityId === cityId) {
+//     walkersInCity.push(walker);
+//    }
+// }
+
+//     for (const walker of walkersInCity) {
+//         walkerNames.push(walker.name);
+//     }
+//     window.alert(`${walkerNames.join(", ")} is servicing this city.`)
+// }
+// )
 
 export const CityList = () => {
-    let citiesHTML = "<ul>"
+  let citiesHTML = "<ul>";
 
-    for (const city of cities) {
-        citiesHTML += `<li data-type="city" data-cityid="${city.id}">${city.name}</li>`
-    }
+  for (const city of cities) {
+    citiesHTML += `<li data-type="city" data-cityid="${city.id}">${city.name}</li>`;
+  }
 
-    citiesHTML += "</ul>"
+  citiesHTML += "</ul>";
 
-    return citiesHTML
-}
+  return citiesHTML;
+};
